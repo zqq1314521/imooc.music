79233e43a9660820470c7c7b583a63bc9f072558
zqq1314521 <loveibeyond@gmail.com>
2015-04-05 17:03:56 +0800

    Topbar&PlayPart Code is edited.

--- src/com/imooc/music/ui/MainActivity.java
+++ src/com/imooc/music/ui/MainActivity.java
@@ -1,39 +1,146 @@
 package com.imooc.music.ui;
 
-import com.imooc.music.R;
-import com.imooc.music.R.id;
-import com.imooc.music.R.layout;
-import com.imooc.music.R.menu;
-
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.opengl.Visibility;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.animation.Animation;
+import android.view.animation.Animation.AnimationListener;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LinearInterpolator;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
-public class MainActivity extends ActionBarActivity {
+import com.imooc.music.R;
+
+public class MainActivity extends Activity implements OnClickListener {
+
+	// 唱片相关动画
+
+	private Animation mPanAnim;
+	private LinearInterpolator mPanLin;
+
+	private Animation mBarInAnim;
+	private LinearInterpolator mBarInLin;
+
+	private Animation mBarOutAnim;
+	private LinearInterpolator mBarOutLin;
+
+	// 播放按钮的触发
+	private ImageButton mPlayButton;
+	private ImageView mPlayBar;
+	private ImageView mDiskLight;
+	// 是否在播放状态
+	private boolean isPlaying = false;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		mPanAnim = AnimationUtils.loadAnimation(this, R.anim.rotate);
+		mPanLin = new LinearInterpolator();
+		mPanAnim.setInterpolator(mPanLin);
+		mPanAnim.setAnimationListener(new AnimationListener() {
+
+			@Override
+			public void onAnimationStart(Animation animation) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void onAnimationRepeat(Animation animation) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void onAnimationEnd(Animation animation) {
+				// TODO Auto-generated method stub
+				mPlayBar.startAnimation(mBarOutAnim);
+			}
+		});
+
+		mBarInAnim = AnimationUtils.loadAnimation(this, R.anim.rotate_45);
+		mBarInLin = new LinearInterpolator();
+		mBarInAnim.setInterpolator(mBarInLin);
+		mBarInAnim.setFillAfter(true);
+		mBarInAnim.setAnimationListener(new AnimationListener() {
+
+			@Override
+			public void onAnimationStart(Animation animation) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void onAnimationRepeat(Animation animation) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void onAnimationEnd(Animation animation) {
+				// TODO Auto-generated method stub
+				mDiskLight.startAnimation(mPanAnim);
+			}
+		});
+
+		mBarOutAnim = AnimationUtils.loadAnimation(this, R.anim.rotate_d_45);
+		mBarOutLin = new LinearInterpolator();
+		mBarOutAnim.setInterpolator(mBarOutLin);
+		mBarOutAnim.setFillAfter(true);
+		mBarOutAnim.setAnimationListener(new AnimationListener() {
+
+			@Override
+			public void onAnimationStart(Animation animation) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void onAnimationRepeat(Animation animation) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void onAnimationEnd(Animation animation) {
+				// TODO Auto-generated method stub
+				isPlaying = false;
+				mPlayButton.setVisibility(View.VISIBLE);
+			}
+		});
+
+		mPlayButton = (ImageButton) findViewById(R.id.ibt_play);
+		mPlayButton.setOnClickListener(this);
+
+		mPlayBar = (ImageView) findViewById(R.id.iv_playbar);
+		mDiskLight = (ImageView) findViewById(R.id.iv_disk_light);
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	public void onClick(View v) {
+		handlePlayButton();
 	}
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
+	// 动画启动
+	private void handlePlayButton() {
+		if (mPlayButton != null) {
+			if (!isPlaying) {
+				isPlaying = true;
+				mPlayButton.setVisibility(View.INVISIBLE);
+				mPlayBar.startAnimation(mBarInAnim);
+			}
 		}
-		return super.onOptionsItemSelected(item);
+	}
+//	对于中断事件的处理
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		mDiskLight.clearAnimation();
+		super.onPause();
 	}
 }
